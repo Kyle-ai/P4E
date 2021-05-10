6c19a199bb078618a2d25a68339907fa3cfe1132
Kyle <>
2021-05-09 22:33:45 -0600

    finished database chapter

--- Misc.py/autograder.py
+++
@@ -1,8 +0,0 @@
-fname = input('Enter file name: ')
-if ( len(fname) < 1 ) : fname = 'mbox.txt'
-fh = open(fname)
-for line in fh:
-	if not line.startswith('From: ') : continue
-	pieces = line.split()[1]
-	org = pieces.split('@')[1]
-    print(org)
--- README.md
+++
@@ -1,1 +0,0 @@
-This is my run through of the book Python For Everyone. Included will be the projects from the course on freecodecamp data exploration course.
